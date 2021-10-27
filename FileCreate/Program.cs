234244b9d07c3be3d2df18eecf687485985246db
Opilane TTHK <opilane@tthk.ee>
2021-10-27 11:32:01 +0300

    add read from file

---
+++ FileCreate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace FileCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            string userfile = "wishlist";
+            string fullFilePath = $@"{rootDirectory}\{userfile}.txt";
+
+            bool directoryExists = Directory.Exists(rootDirectory);
+            bool fileExists = File.Exists(fullFilePath);
+
+            if(directoryExists && fileExists)
+            {
+                Console.WriteLine($"{userfile} already exists in {rootDirectory}");
+            }
+            else if (!directoryExists)
+            {
+                Console.WriteLine($"{rootDirectory} does not exist.");
+                Directory.CreateDirectory(rootDirectory);
+                Console.WriteLine($"{rootDirectory} has been created.");
+                File.Create(fullFilePath);
+                Console.WriteLine($"{userfile}.txt has been created in {rootDirectory}");
+            }
+            else
+            {
+                File.Create(fullFilePath);
+                Console.WriteLine($"{userfile}.txt has been created in {rootDirectory}");
+            }
+
+            
+        }
+    }
+}
---
+++ ReadingFromFile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\samples";
+            string userfile = "wishlist";
+            string fullFilePath = $@"{rootDirectory}\{userfile}.txt";
+
+            string[] dataFromFile = File.ReadAllLines(fullFilePath);
+
+            foreach(string line in dataFromFile)
+            {
+                Console.WriteLine(line);
+            }
+
+            dataFromFile[0] = "pink unicorn";
+
+            foreach (string line in dataFromFile)
+            {
+                Console.WriteLine(line);
+            }
+            File.WriteAllLines(fullFilePath, dataFromFile);
+        }
+    }
+}
